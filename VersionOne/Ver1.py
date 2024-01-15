78822fab4abec870cd2ac0e0694e4a1ae62a4c84
Shithila Urshi <78228551+ushithila@users.noreply.github.com>
2024-01-15 16:21:19 -0500

    Add files via upload

--- VersionOne/Ver1.py
+++ VersionOne/Ver1.py
@@ -8,11 +8,15 @@ def rando():
     while(myString != x):
         print("Wrong! Guess again!")
         myString = int(input("Enter number to guess: "))
-    
     print("Correct! The number is", x)
 
 def main():
     rando()
+    choice = input("Play again?: ")
+    while(choice.lower() == "yes"):
+        rando()
+        choice = input("Play again?: ")
+        
 
 if __name__ == '__main__':
     main()
